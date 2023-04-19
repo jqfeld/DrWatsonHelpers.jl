3e933d8d53c42fb4e02c9a8433dd1f248be64966
Jan Kuhfeld <jan.kuhfeld@rub.de>
2023-04-19 22:15:49 +0900

    removed outdated function, added simple metadata

--- src/DrWatsonHelpers.jl
+++ src/DrWatsonHelpers.jl
@@ -1,9 +1,43 @@
 module DrWatsonHelpers
 
-include("session.jl")
-export current_session, start_session, makeplot
+export msave, make_metadata
 
-include("themes.jl")
-export report_theme, paper_theme, palette
+import DrWatson: projectname, gitdescribe, wsave
+import Dates: today
+default_metadata() = Dict(
+    :project => projectname(),
+    :gitcommit => gitdescribe(),
+    :date => today()
+)
+function make_metadata(_::Any)
+    Dict{Symbol,Any}()
+    # default_metadata()
+end
+
+import DifferentialEquations: ODESolution
+
+function make_metadata(sol::ODESolution)
+    # metadata = default_metadata()
+    metadata = Dict(
+            :initial_values => Dict( sol.prob.f.syms .=> sol.prob.u0),
+            :parameters => Dict(sol.prob.f.paramsyms .=> sol.prob.p),
+        )
+    return metadata
+end
+
+function make_metadata(dict::Dict)
+    ks = keys(dict)
+    vs = values(dict)
+    metadata = Dict(ks .=> make_metadata.(vs))
+end
+
+import DrWatson: wsave
+import YAML
+function msave(path, obj; metadata=nothing )
+    metadata = metadata === nothing ? default_metadata() : merge(metadata, default_metadata())
+    # merge!(metadata, make_metadata(obj))
+    wsave(path, obj)
+    YAML.write_file(path * ".yml", metadata)
+end
 
 end # module
